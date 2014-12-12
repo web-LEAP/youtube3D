d480abed795d105f60c23d7b7be69dc7020158f5
Anand Singh Kunwar <anandkunwar95@gmail.com>
2014-12-13 03:24:13 +0530

    hand only appears for an instant

--- addon/3dfox/lib/main.js
+++ addon/3dfox/lib/main.js
@@ -1,17 +1,35 @@
-var self = require("sdk/self");
-
-var button = require("sdk/ui/button/action").ActionButton({
-    id: "style-tab",
-    label: "Style Tab",
-    icon: "./icon-16.png",
-    onClick: function() {
-        require("sdk/tabs").activeTab.attach({
-            contentScriptFile: [ self.data.url("./three.js"),
-                self.data.url("./leap.js"),
-                self.data.url("./leap-plugins-0.1.6.1.js"),
-                self.data.url("./leap.rigged-hand-0.1.3.min.js.js"),
-                self.data.url("./showhand.js"),
-                self.data.url("./abc.js") ]
-        });
-    }
+var buttons = require('sdk/ui/button/action');
+var tabs = require("sdk/tabs");
+var pageMod = require("sdk/page-mod");
+var data = require("sdk/self").data;
+var button = buttons.ActionButton({
+    id: "mozilla-link",
+    label: "Visit Mozilla",
+    icon: {
+        "16": "./icon-16.png"
+    },
+    onClick: handleClick
 });
+
+pageMod.PageMod({
+    include: "*" ,
+    contentScriptFile: [ data.url("./three.js"),
+    data.url("./leap.js"),
+    data.url("./leap-plugins-0.1.6.1.js"),
+    data.url("./leap.rigged-hand-0.1.3.min.js"),
+    data.url("./showhand.js"),
+    data.url("./abc.js") ]
+//     contentScript: 'document.body.innerHTML = ' +
+ //                   ' "<h1>Page matches ruleset</h1>";'
+});
+
+function handleClick(state) {
+    //tabs.open("https://www.mozilla.org/");
+    tabs.open({
+        url: "https://www.google.co.in/",
+        onReady: function onReady(tab) {
+            console.log(tab.title);
+        }
+    });
+    //console.log("hello baby i'm printing");
+}
