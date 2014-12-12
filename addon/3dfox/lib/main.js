134a93f3d6473a73df817eb25b6bbaad54c779ff
Anand Singh Kunwar <anandkunwar95@gmail.com>
2014-12-13 02:36:23 +0530

    Some Testing

--- addon/3dfox/lib/main.js
+++ addon/3dfox/lib/main.js
@@ -1,33 +1,17 @@
-var buttons = require('sdk/ui/button/action');
-var tabs = require("sdk/tabs");
-var pageMod = require("sdk/page-mod");
-var data = require("sdk/self").data;
+var self = require("sdk/self");
 
-var button = buttons.ActionButton({
-  id: "mozilla-link",
-  label: "Visit Mozilla",
-  icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
-  },
-  onClick: handleClick
+var button = require("sdk/ui/button/action").ActionButton({
+    id: "style-tab",
+    label: "Style Tab",
+    icon: "./icon-16.png",
+    onClick: function() {
+        require("sdk/tabs").activeTab.attach({
+            contentScriptFile: [ self.data.url("./three.js"),
+                self.data.url("./leap.js"),
+                self.data.url("./leap-plugins-0.1.6.1.js"),
+                self.data.url("./leap.rigged-hand-0.1.3.min.js.js"),
+                self.data.url("./showhand.js"),
+                self.data.url("./abc.js") ]
+        });
+    }
 });
-
-pageMod.PageMod({
-  include: "file:///home/aadil/Desktop/h.html*",
-  contentScriptFile: [data.url("three.js"), data.url("leap.js"), data.url("leap_plugins.js"), data.url("leap.rigged_hand.min.js"), data.url("display_hand.js")]
-  // contentScript: 'document.body.innerHTML = ' +
-  //                ' "<h1>Page matches ruleset</h1>";'
-});
-
-function handleClick(state) {
-  //tabs.open("https://www.mozilla.org/");
-  tabs.open({
-  url: "https://www.google.co.in/",
-  onReady: function onReady(tab) {
-    console.log(tab.title);
-  }
-});
-  //console.log("hello baby i'm printing");
-}
